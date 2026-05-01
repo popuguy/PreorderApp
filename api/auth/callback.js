9a3264780a10a6b76558f99ac253a22ea92b34a2
popuguy <paulcantr@gmail.com>
2026-05-01 18:54:10 -0400

    Add detailed auth callback error output

--- api/auth/callback.js
+++ api/auth/callback.js
@@ -32,6 +32,6 @@ export default async function handler(req, res) {
     res.end();
   } catch (error) {
     console.error(error);
-    res.status(500).send("Unable to complete Shopify authorization.");
+    res.status(500).send(`Unable to complete Shopify authorization: ${error.message}`);
   }
 }
